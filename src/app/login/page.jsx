e38dab58f46d7e9ddb95ef38e46f7235a92fd898
Adithyan S Pillai <adithyanspillaiofficial@gmail.com>
2026-01-09 04:47:41 +0530

    A fix to a built error : Added AuthForm in Suspanse

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,13 @@
 import AuthForm from '@/components/AuthForm'
+import Loading from '@/widgets/Loading'
 import React from 'react'
 
 function page() {
   return (
     <div>
+      <Suspense fallback={<Loading />}>
         <AuthForm />
+      </Suspense>
     </div>
   )
 }
